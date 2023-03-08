42b7be507bd19ebebd48c64068b3a9392b113991
mt1022 <mt1022@users.noreply.github.com>
2023-03-08 17:41:24 +0800

    fix bug caused by tx_name w/ underscore

--- orf_type.py
+++ orf_type.py
@@ -108,7 +108,7 @@ def get_giv_boundary(giv_str):
 def read_price(path):
     """Read PRICE results"""
     price = pd.read_table(path)
-    price['tx_name'] = price.Id.str.split('_', expand=True)[0]
+    price['tx_name'] = price.Id.str.rsplit('_', n=2, expand=True)[0]
     price[['chrom_strand', 'givs']] = price.Location.str.split(':', expand=True)
     price['chrom'] = price.chrom_strand.str[:-1]
     price['strand'] = price.chrom_strand.str[-1]
